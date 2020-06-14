d975f97bfd46232d40de0235ec85087f1d97c06e
Oyewole ABAYOMI S <oyewoleabayomi@gmail.com>
2020-06-14 17:27:21 +0100

    add subdomain middleware

---
+++ src/middleware/SubDomainMiddleware.ts
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+
+import ResUtil from '../Utils/RespUtil';
+import { Request, Response, NextFunction } from 'express';
+import { HttpStatus } from '../types/enums/HttpStatus';
+import { getPartnerRepository } from '../database/repository/index';
+
+const util = new ResUtil();
+// { [key: string]: string; }
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const subdomain = String(req.headers['X-Subdomain']);
+    if (!subdomain) return next();
+
+    const { findSubDomain } = getPartnerRepository();
+    const partner = await findSubDomain(subdomain);
+
+    Object.assign(req, { partner });
+
+    next();
+  } catch ({ statusCode, message }) {
+    return util.setError(statusCode || HttpStatus.BAD_REQUEST, message).send(res);
+  }
+};
